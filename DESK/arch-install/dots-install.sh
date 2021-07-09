fff2dd174ff188af25a3a116fdddd0721924e6ee
maxprio <maxprio@gmail.com>
2021-07-09 13:51:51 +0500

    just moving files around.

--- DESK/arch-install/dots-install.sh
+++ DESK/arch-install/dots-install.sh
@@ -9,13 +9,14 @@ git clone --bare $DOTS_REP_URL $DOTS_DIR
 dots-git-cmd () {
    /usr/bin/git --git-dir=$DOTS_DIR/--work-tree=$HOME $@
 }
-dots-git-cmd checkout
+dots-git-cmd checkout 2>&1
 if [ $? = 0 ]; then
   echo "Checked out dots.";
   else
     echo "Backing up pre-existing dots files.";
   mkdir -p "$BKP_DIR" 
   dots-git-cmd checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} "$BKP_DIR"/{}
+  dots-git-cmd checkout
 fi;
-dots-git-cmd checkout
 dots-git-cmd config status.showUntrackedFiles no
+dots-git-cmd remote rename origin github
