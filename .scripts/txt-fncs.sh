981bd5fc24c8abbaa2ad6f72738d0015b7813f70
maxprio <maxprio@gmail.com>
2023-02-04 01:46:57 +0500

    unspecified commit

--- .scripts/txt-fncs.sh
+++ .scripts/txt-fncs.sh
@@ -10,3 +10,21 @@ sed -e 's/#.*$//;/^\s*$/d;s/\s\+$//' $src
 
 # one WORD per line
 fout-hash $1 | sed 's/\s\+/\n/g' -
+
+cut_out_core () {
+# gets rid of the file ($1) content, between the given pattern lines($2$3) .
+    sed  "/$2/q" $1
+    sed -n "/$3/,\$p" $1
+}
+cut_core () {
+# saves the file ($1) content, between the given pattern lines($2$3) .
+    sed -n "/$3/,\$p" $1 \
+      | sed  "/$2/q" $1
+}
+injection () {
+# inserts the first file content, into the second, in front of  the pattern line ($3) .
+    sed -n "/$3/q;p" $2
+    cat $1
+    sed -n "/$3/,\$p" $2
+}
+
--- DESK/arch-setup/vim-setup.sh
+++ DESK/arch-setup/vim-setup.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-paru -S --noconfirm --needed curl vim xkb-switch
+echo -ne '
+SETTING-UP VIM.
+---------------'
+# just in case
+#paru -s --noconfirm --needed curl vim xkb-switch
 
-# SETTING-UP THE "PLUG" PLUGIN MANAGER AND PLUGINS:
-#--------------------------------------------------
+# a finction,to cut out a function
+cut_core () {
+# filters out the lines, outside the given pattern lines ( $1 & $2 ).
+    sed -n "/$1/,\$p" - \
+      | sed  "/$2/q" -
+}
+
+echo -ne '
+setting-up the plugin manager "vim-plug" 
+-----------------------------------------'
 plug_url="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 vimrc_url="https://raw.githubusercontent.com/maxprio/dots/main/.vimrc"
 
@@ -11,22 +23,42 @@ vimrc_url="https://raw.githubusercontent.com/maxprio/dots/main/.vimrc"
 [ ! -f '~/.vim/autoload/plug.vim' ]\
     && curl -fLo ~/.vim/autoload/plug.vim --create-dirs "$plug_url"
 
-# download config to a temp file
-[ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.bak # just in case
-curl -fLo ~/.vimrc.tmp "$vimrc_url"
+echo -ne '
+Done!
+-----'
 
-
-# begin the config with the autocommand on enter.
-# Run PlugInstall if there are missing plugins and quit
+echo -ne '
+setting-up plugins.
+-------------------'
+echo -ne 'creating the temp config...  '
+# just in case
+[ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.bak
+# begin the local config with the autocommand on enter:
+# run PlugInstall if there are missing plugins and quit
 cat << 'EOF' > ~/.vimrc
 autocmd VimEnter * if len(filter(values(g:plugs), '!isdirectory(v:val.dir)'))
   \| PlugInstall --sync | source $MYVIMRC | quitall
 \| endif
 EOF
-# add the rest of the config
-cat ~/.vimrc.tmp >> ~/.vimrc
+# download the plug function from the config on github.
+# and append to the local config.
+curl -s "$vimrc_url"\
+  | cut_core '.*call\ plug#begin.*' '.*call\ plug#end.*'\
+  | sed '/^\s.*\"/d'\
+  >> ~/.vimrc
+echo -ne ' Done!
+'
 # run vim
+echo -ne '
+running vim to insall the plugins...'
 vim
-# restore the original .vimrc
+
+# clean up
+echo -ne '
+cleaning up...  '
 rm ~/.vimrc
-mv ~/.vimrc.tmp  ~/.vimrc
+[ -f ~/.vimrc.buk ]\
+  && mv ~/.vimrc.tmp  ~/.vimrc
+
+echo -ne ' Done!
+'
